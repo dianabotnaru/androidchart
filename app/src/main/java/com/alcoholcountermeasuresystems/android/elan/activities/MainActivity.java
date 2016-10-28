d6e2fb57484ef0f96f88e95deb8e94fafb084409
diana botnaru <botunaru.diana@yahoo.com>
2016-10-28 18:51:50 +0200

    added registered button and some action in main activity

--- app/src/main/java/com/alcoholcountermeasuresystems/android/elan/activities/MainActivity.java
+++ app/src/main/java/com/alcoholcountermeasuresystems/android/elan/activities/MainActivity.java
@@ -3,6 +3,8 @@ package com.alcoholcountermeasuresystems.android.elan.activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.alcoholcountermeasuresystems.android.elan.MainApplication;
@@ -15,15 +17,36 @@ import butterknife.OnClick;
 
 public class MainActivity extends BaseActivity {
 
-    @BindString(R.string.home_title)
-    String mTitleString;
-
     @BindView(R.id.text_toolbar_title)
     TextView mToolbarTitleText;
+
+    @BindView(R.id.button_register)
+    Button mRegisterButton;
+
+    @BindView(R.id.layout_breath_test)
+    RelativeLayout mBreathTestLayout;
 
     @OnClick(R.id.button_action)
     void onToolbarActionPressed() {
     }
+
+    @OnClick(R.id.button_register)
+    void onRegisterButtonPressed() {
+        notRegistered=(!notRegistered);
+        initRegisterState(notRegistered);
+    }
+
+    @BindString(R.string.home_title)
+    String mTitleString;
+
+    @BindString(R.string.home_registered_button_title)
+    String mRegisteredButtonString;
+
+    @BindString(R.string.home_notregistered_button_title)
+    String mNotRegisteredButtonString;
+
+
+    private boolean notRegistered; //QA purpose
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,9 +56,19 @@ public class MainActivity extends BaseActivity {
         initViews();
     }
 
-    void initViews(){
+    private void initViews(){
         mToolbarTitleText.setText(mTitleString);
+        initRegisterState(true);
     }
+    private void initRegisterState(boolean notRegistered){
+        mBreathTestLayout.setEnabled(!notRegistered);
+        if (notRegistered){
+            mRegisterButton.setText(mNotRegisteredButtonString);
+        }else {
+            mRegisterButton.setText(mRegisteredButtonString);
+        }
+        mRegisterButton.setEnabled(notRegistered);
+    }       
     @Override
     protected void injectComponents() {
         MainApplication.getAppComponent().inject(this);
